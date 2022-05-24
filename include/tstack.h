3f52358f03f35aee4d471a9fd71822a48f686b61
KarlesaM <99256703+KarlesaM@users.noreply.github.com>
2022-05-24 20:34:58 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -1,9 +1,31 @@
-// Copyright 2021 NNTU-CS
-#ifndef INCLUDE_TSTACK_H_
-#define INCLUDE_TSTACK_H_
-
 template<typename T, int size>
 class TStack {
+ private:
+  T arr[100];
+  int t;
 
+ public:
+  TStack() : t(-1) { }
+  bool isEmpty() const {
+    return t == -1;
+  }
+  bool isFull() const {
+    return t == size - 1;
+  }
+  void push(T value) {
+  if (isFull())
+    throw "Full!";
+  else
+    arr[++t] = value;
+  }
+  T get() const {
+    return arr[t];
+  }
+  void pop() {
+    if (!isEmpty())
+      --t;
+    else
+      throw "Empty!";
+  }
 };
 #endif  // INCLUDE_TSTACK_H_
