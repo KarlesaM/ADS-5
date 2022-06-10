24c0fed52fc779e7cab57db522223816bdbddcd1
KarlesaM <99256703+KarlesaM@users.noreply.github.com>
2022-06-10 16:02:58 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,109 +1,100 @@
 // Copyright 2021 NNTU-CS
-
 #include <map>
 #include "tstack.h"
 
-int priority(char ch) {
-  switch (ch) {
-    case '(':
-      return 0;
-    case ')':
-      return 1;
-    case '+':
-      return 2;
-    case '-':
-      return 2;
-    case '*':
-      return 3;
-    case '/':
-      return 3;
-  }
-  return -1;
-}
-
-bool isDigit(std::string pref) {
-  for (size_t a = 0; a < pref.size(); ++a) {
-    if (pref[a] < '0' || pref[a] > '9')
-      return false;
-  }
-  return true;
+int prior(char total) {
+    if (total == '*' || total == '/') {
+        return 2;
+    } else if (total == '+' || total == '-') {
+        return 1;
+    } else {
+        return 0;
+    }
 }
 
 std::string infx2pstfx(std::string inf) {
   // добавьте код
   return std::string("");
-  TStack <char, 100> itstack;
-  std::string res;
-  for (size_t a = 0; a < inf.size(); a++) {
-    if ((priority(inf[a]) == -1) && (priority(inf[a]) != 1)) {
-      if (!res.empty() && priority(inf[a - 1]) != -1) {
-        res.push_back(' ');
-      }
-      res.push_back(inf[a]);
-    } else if ((priority(inf[a]) > priority(itstack.get()))
-               || (itstack.isEmpty()) || (priority(inf[a]) == 0)) {
-      itstack.push(inf[a]);
-    } else {
-      if (priority(inf[a]) == 1) {
-        while (priority(itstack.get()) != 0) {
-          res.push_back(' ');
-          res.push_back(itstack.get());
-          itstack.pop();
+    std::string result = "";
+    TStack<char, 100> st;
+    for (int a = 0; a < inf.length(); a++) {
+        char b = inf[a];
+        if (b >= '0' && b <= '9') {
+            result += b;
+            result += ' ';
+        } else if (b == '(') {
+            st.push(b);
+        } else if (b == ')') {
+            while (st.top() != '(') {
+                result += st.pop();
+                result += ' ';
+            }
+            st.pop();
+        } else {
+            while (!st.isEmpty() && prior(b) <= prior(st.top())) {
+                result += st.pop();
+                result += ' ';
+            }
+            st.push(b);
         }
-        itstack.pop();
-      } else {
-        while (priority(itstack.get()) >= priority(inf[a])) {
-          res.push_back(' ');
-          res.push_back(itstack.get());
-          itstack.pop();
-        }
-        itstack.push(inf[a]);
-      }
     }
-  }
-  while (!itstack.isEmpty()) {
-    res.push_back(' ');
-    if (priority(itstack.get()) != 0) {
-      res.push_back(itstack.get());
+    while (!st.isEmpty()) {
+        result += st.pop();
+        result += ' ';
     }
-    itstack.pop();
-  }
-  return res;
+    result = result.substr(0, result.length() - 1);
+    return result;
+}
+
+int charConv(char b) {
+    switch (b) {
+        case '1': return 1; break;
+        case '2': return 2; break;
+        case '3': return 3; break;
+        case '4': return 4; break;
+        case '5': return 5; break;
+        case '6': return 6; break;
+        case '7': return 7; break;
+        case '8': return 8; break;
+        case '9': return 9; break;
+        default: return 0; break;
+    }
 }
 
 int eval(std::string pref) {
   // добавьте код
   return 0;
 int eval(std::string post) {
-  TStack <int, 100> ptstack;
-  std::string temp;
-  int op1 = 0;
-  int op2 = 0;
-  size_t start = 0, end = 0;
-  for (size_t a = 0; a < post.size(); ++a) {
-    if (post[a] == ' ' || a == post.size()-1) {
-      end = a;
-      if (a == post.size() - 1)
-        end++;
-      temp = post.substr(start, end - start);
-      start = end + 1;
-      if (isDigit(temp)) {
-        ptstack.push(std::stoi(temp));
-      } else {
-        oper2 = ptstack.get();
-        ptstack.pop();
-        oper1 = ptstack.get();
-        ptstack.pop();
-        if (temp == "+")
-          ptstack.push(oper1 + oper2);
-        else if (temp == "-")
-          ptstack.push(oper1 - oper2);
-        else if (temp == "*")
-          ptstack.push(oper1 * oper2);
-        else if (temp == "/")
-          ptstack.push(oper1 / oper2);
-      }
+    std::string post1 = post;
+    int count = 0;
+    for (int a = 0; a < post.length(); a++) {
+        if (post[a] == ' ') {
+            continue;
+        } else {
+            post1[count] = post[a];
+            count++;
+        }
     }
-  }
-  return ptstack.get();
+    TStack<char, 100> st;
+    for (int a = 0; a < post1.length(); a++) {
+        char b = post1[a];
+        if (b == ' ') {
+            break;
+        } else if (b >= '0' && b <= '9') {
+            st.push(charConv(b));
+        } else {
+            int num1 = st.pop();
+            int num2 = st.pop();
+            if (b == '+') {
+                st.push(num2 + num1);
+            } else if (b == '-') {
+                st.push(num2 - num1);
+            } else if (b == '/') {
+                st.push(num2 / num1);
+            } else if (b == '*') {
+                st.push(num2 * num1);
+            }
+        }
+    }
+    return st.top();
 }
